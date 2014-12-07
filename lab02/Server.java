479d01999e134d9cb94818be7b5a8ac30c906e32
Vocryan Koverald <yzx19942013@gmail.com>
2014-12-07 21:06:12 +0800

    add server

---
+++ lab02/Server.java
@@ -0,0 +1,90 @@
+package lab02;
+import java.io.*;
+import java.net.*;
+import java.util.concurrent.*;
+
+public class Server {
+	private static Database db = new Database();
+
+	public static void main( String[] args ) {
+		try {
+			ServerSocket serverSocket = new ServerSocket(23333);
+			int clientNo = 1;
+			while (true) {
+				Socket connectToClient = serverSocket.accept();
+				System.out.println("Start thread for client " + clientNo);
+				InetAddress clientInetAddress = connectToClient.getInetAddress();
+				
+				System.out.println("Client " + clientNo + "\'s hostname is " +
+						clientInetAddress.getHostName());
+
+				System.out.println("Client " + clientNo + "\'s IP Address is " +
+						clientInetAddress.getHostAddress());
+				System.out.println("Client " + clientNo + "\'s port is " +
+						connectToClient.getPort());
+				HandleTask task = new HandleTask(connectToClient);
+				new Thread(task).start();
+
+				clientNo++;
+			}
+		} catch (IOException ex) {
+			System.err.println(ex);
+		}
+	}
+
+	public static class HandleTask implements Runnable {
+		private Socket connectionSocket;
+
+		public HandleTask(Socket socket) {
+			this.connectionSocket = socket;
+		}
+		
+		public void run() {
+			try {
+				DataInputStream isFromClient = new DataInputStream(
+						connectionSocket.getInputStream());
+				DataOutputStream osToClient = new DataOutputStream(
+						connectionSocket.getOutputStream());
+				while (true) {
+					String buf = isFromClient.readUTF();
+					String [] s = buf.substring(3).split("\\^");
+					String opcode = buf.substring(1, 3);
+					if (buf.charAt(0) == 'q') {
+						if (opcode.equals("li")) {
+							boolean result = db.login(s[0], s[1],
+								connectionSocket.getInetAddress(),
+								connectionSocket.getPort());
+							osToClient.writeUTF("rli"+result);
+							System.out.println(result);
+						} else if (opcode.equals("rg")) {
+							boolean result = db.register(s[0], s[1]);
+							osToClient.writeUTF("rrg" + result);
+							System.out.println(result);
+						} else if (opcode.equals("lo")) {
+							boolean result = db.logout(s[0]);
+							osToClient.writeUTF("rlo" + result);
+							System.out.println(result);
+						} else if (opcode.equals("se")) {
+							String result = db.request(s[0]);
+							osToClient.writeUTF("rse" + result);
+							System.out.println(result);
+						} else if (opcode.equals("za")) {
+							boolean result = db.clickZan(s[0],s[1],s[2]);
+							osToClient.writeUTF("rza" + result);
+							System.out.println(result);
+						} else if (opcode.equals("uz")) {
+							boolean result = db.clickUnzan(s[0], s[1], s[2]);
+							osToClient.writeUTF("ruz" + result);
+							System.out.println(result);
+						}
+					}
+
+				}
+			} catch (Exception ex) {
+				System.err.println(ex);
+			}
+		}
+	}
+	
+
+}
